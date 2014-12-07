3ca39a8d12a3256c9bab21fc90af858f5c23c01a
lorus <lorus@localhost>
2014-12-07 18:32:02 +0600

    Upgrade build script

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,7 +63,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-wintersmith');
+  grunt.loadNpmTasks('grunt-wintersmith-run');
 
   grunt.registerTask(
     'default',
